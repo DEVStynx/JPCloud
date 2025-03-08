b035d8b870e24cf8c44bab7b393794b420818ddc
DEVStynx <131193709+DEVStynx@users.noreply.github.com>
2025-03-08 12:32:19 +0100

    Updated Repositories

--- JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/db/model/file/FileRepository.java
+++ JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/db/model/file/FileRepository.java
@@ -1,9 +1,10 @@
 package de.stynxyxy.jpcloudbackend.db.model.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(exported = false)
+@RepositoryRestResource(exported = false)
 public interface FileRepository extends JpaRepository<FileEntity,Long> {
     public FileEntity findByName(String name);
 }
--- JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/db/model/user/UserRepository.java
+++ JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/db/model/user/UserRepository.java
@@ -1,9 +1,10 @@
 package de.stynxyxy.jpcloudbackend.db.model.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(exported = false)
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
     public UserEntity findByUsername(String username);
 
