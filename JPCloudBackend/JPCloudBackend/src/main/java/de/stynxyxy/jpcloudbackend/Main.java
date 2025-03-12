6fa103b0d290ebb016b3d83463240d7ad0847711
DEVStynx <131193709+DEVStynx@users.noreply.github.com>
2025-03-12 15:17:32 +0100

    Reformatted Code

--- JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/Main.java
+++ JPCloudBackend/JPCloudBackend/src/main/java/de/stynxyxy/jpcloudbackend/Main.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+    public static String sourcePath = "src\\main\\resources\\cloud";
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-    public static String sourcePath = "src\\main\\resources\\cloud";
+
+
 }
