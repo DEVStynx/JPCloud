1891586910fc373fea52854409c6d558238daea2
Jonas <131193709+DEVStynx@users.noreply.github.com>
2025-04-03 07:02:21 +0200

    Delete JPCloudFrontend/cmake-build-debug directory

--- JPCloudFrontend/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_JPCloudFrontend_FILE C:/Users/jonas/Desktop/Development/JPCloud/JPCloudFrontend/cmake-build-debug/JPCloudFrontend.exe)
-set(__QT_DEPLOY_TARGET_JPCloudFrontend_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_JPCloudFrontend_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
